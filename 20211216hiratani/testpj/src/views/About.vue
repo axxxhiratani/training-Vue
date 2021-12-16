2bba02667e49f8e7071a31397706e897d8f8db64
axxxhiratani <axxx.hiratani@gmail.com>
2021-12-16 10:53:45 +0900

    test

---
+++ 20211216hiratani/testpj/src/views/About.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="about">
+    <h1>
+      current team: {{searchName(teamId)}}
+    </h1>
+    <p>
+      <button @click="countUp(searchName(teamId))">counter</button>
+    </p>
+    <p>
+      {{$store.state.sum}}
+    </p>
+    <p>
+      Last team name clicked: {{$store.state.lastTeam}}
+    </p>
+    <p class="back" @click="back">
+      戻る
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      teams: [
+        {
+          id: "c36136ef-552f-4dc9-84b6-65ecb8574e81",
+          name: "Team_A",
+        },
+        {
+          id: "ed0459f5-716d-4ef4-99ba-5dc750dfe0d0",
+          name: "Team_B",
+        },
+        {
+          id: "60d0d5d5-8ce9-41d9-a65b-13a090c10ae1",
+          name: "Team_C",
+        },
+      ]
+    };
+  },
+  props:[
+    "teamId"
+  ],
+  methods:{
+    searchName(id){
+      let name = "";
+      this.teams.forEach(value =>{
+        if(value.id === id){
+          name = value.name;
+        }
+      });
+      return name;
+    },
+    countUp(name){
+      this.$store.commit("countUp",name);
+    },
+    back(){
+      this.$router.push({name:"Home"});
+    }
+  }
+};
+</script>
+
+<style scoped>
+.back{
+  cursor: pointer;
+}
+</style>
---
+++ 20211216hiratani/testpj/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <h2>チーム一覧</h2>
+    <div v-for=" team in teams" :key="team.id">
+      <a @click="userPage(team.id)">{{team.name}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      teams: [
+        {
+          id: "c36136ef-552f-4dc9-84b6-65ecb8574e81",
+          name: "Team_A",
+        },
+        {
+          id: "ed0459f5-716d-4ef4-99ba-5dc750dfe0d0",
+          name: "Team_B",
+        },
+        {
+          id: "60d0d5d5-8ce9-41d9-a65b-13a090c10ae1",
+          name: "Team_C",
+        },
+      ],
+    };
+  },
+  methods:{
+    userPage(id){
+      this.$router.push({name:"About",params:{teamId:id}});
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  line-height: 30px;
+}
+a{
+  list-style: none;
+  cursor: pointer;
+}
+.home{
+  text-align: center;
+}
+</style>
